8fc8e9a40e7248dfdbff7ad1335c9b056ff449af
brennocm <brennocmiranda@outlook.com>
2022-10-17 17:53:25 -0300

    Implementa novos exercicios

---
+++ 00-javascript-para-iniciantes/04-boolean-e-condiocionais/script.js
@@ -0,0 +1,45 @@
+// var possuiGraduacao = false;
+
+// if(possuiGraduacao) {
+//   console.log('Possui graduação');
+// } else {
+//   console.log('Não possui graduação');
+// }
+
+var possuiTecnico = false,
+    possuiMestrado = false;
+
+if(possuiMestrado){
+  console.log('Possui graduacao e mestrado');
+} else if(possuiTecnico){
+  console.log('Possui técnico');
+} else {
+  console.log('Possui apenas ensino médio');
+}
+
+var nome = 'Brenno',
+    nome1;
+
+if(nome) {
+  console.log('Nome existe');
+} else{
+  console.log('Nome não existe');
+}
+
+if(nome1) {
+  console.log('Nome existe');
+} else{
+  console.log('Nome não existe');
+}
+
+var x = '10';
+
+if(x == 10){
+  console.log('Verdadeiro');
+}
+
+if(x === 10){
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
